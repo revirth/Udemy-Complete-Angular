a916409e1dc2cdec1d8ca242ff83c6169a597a37
JT <revirth@gmail.com>
2019-02-19 14:09:45 -0500

    Reactive Forms

---
+++ src/app/new-course-form/new-course-form.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { formControlBinding } from '@angular/forms/src/directives/reactive_directives/form_control_directive';
+
+@Component({
+  selector: 'new-course-form',
+  templateUrl: './new-course-form.component.html',
+  styleUrls: ['./new-course-form.component.scss']
+})
+export class NewCourseFormComponent {
+  // form = new FormGroup({
+  //   name: new FormControl('', Validators.required),
+  //   contact: new FormGroup({
+  //     email: new FormControl(),
+  //     phone: new FormControl()
+  //   }),
+  //   topics: new FormArray([])
+  // });
+
+  form;
+  
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ['', Validators.required],
+      contact: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array([])
+    })
+  }
+
+  addTopic(topic: HTMLInputElement) {
+    if(topic.value == '') return;
+
+    this.topics.push(new FormControl(topic.value));
+    topic.value = '';
+  }
+
+  get topics() {
+    return this.form.get('topics') as FormArray;
+  }
+
+  removeTopic(topic: FormControl) {
+    let index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+
+}
